2d494d7e0ef3605c16ead15faa05bc65f7563db3
chihebkema2 <yassermekloub@gmail.com>
2026-07-02 10:36:30 +0100

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,1 +1,22 @@
+"""
+Shared Data Models
+"""
 
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class BoundingBox:
+    x1: float
+    y1: float
+    x2: float
+    y2: float
+
+    def as_list(self) -> List[float]:
+        return [
+            self.x1,
+            self.y1,
+            self.x2,
+            self.y2,
+        ]
